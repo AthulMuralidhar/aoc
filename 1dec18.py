5e89a65739dffc02cbcce44b3209983cb8451760
AthulMuralidhar <athul.m68@gmail.com>
2018-12-02 15:27:33 +0100

    done 1

--- 1dec18.py
+++ 1dec18.py
@@ -4,14 +4,23 @@ using Beautiful Soup implimentation to get data from AOC website
 Language: Python 3+
 AM
 """
-a = []
-with open('inp.txt','r') as file:
-    a = 0
-    while True:
+# import ipdb; ipdb.set_trace()
+b = []
+a = 0
+c=[]
+# b.append(a)
+counter = 0
+
+while True:
+    with open('inp.txt','r') as file:
         for i in file:
             if i.rstrip('\n'):
-                a.append(int(i))
-
-
-print(a)
-# print(b)
+                a+=int(i)
+                b.append(a)
+            if len(b) != len(set(b)):
+                c = b
+                break
+    if c:
+        break
+print(b[-1])
+# print()
