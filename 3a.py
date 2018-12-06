73bb079c4059e773dda5c841e9c40fa8c0ddef83
AthulMuralidhar <athul.m68@gmail.com>
2018-12-06 13:44:34 +0100

    6dec18

---
+++ 3a.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+data = []
+with open('inp.txt','r') as file:
+    for i in file:
+        data.append(i.rstrip())
+
+data = data[:-1]
+# print(data)
+# print(len(data))
+for n,d in enumerate(data):
+    key,carpet = d.split(':')
+    key = key.split('@')[1]
+    key = (int(key.split(',')[0]),int(key.split(',')[1]))
+    carpet = (int(carpet.split('x')[0]),int(carpet.split('x')[1]))
+    data[n] = (key,carpet)
+
+# print(data)
+
+matrix = np.zeros((1500,1500))
+counter = 0
+nan_counter = 0
+
+for d in data:
+    counter +=1
+    for i in range(1500):
+        for j in range(1500):
+            if i in range(d[0][0],d[0][0]+d[1][0]) and j in range(d[0][1],d[0][1]+d[1][1]):
+                    if matrix[j,i] == 0:
+                        matrix[j,i] = counter
+                    elif matrix[j,i] != 0 and matrix[j,i] != np.nan:
+                        nan_counter +=1
+                        matrix[j,i] = np.nan
+                    
+
+print(nan_counter)
