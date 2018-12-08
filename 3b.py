39b5f5b850a415131599e70bcb73d8a40f156763
AthulMuralidhar <athul.m68@gmail.com>
2018-12-08 02:19:32 +0100

    started 4a

--- 3b.py
+++ 3b.py
@@ -1,5 +1,19 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-data = np.genfromtxt('out.txt')
-data = np.trim_zeros(data)
-print(data)
+data = np.genfromtxt('out.txt') # len(data) = 1500
+data = data[~(data==0).all(1)] #len(data) = 999
+uniques = []
+for i in data:
+    for j in set(i):
+        if j != 0 and np.isnan(j) == False:
+            uniques.append(j)
+
+uniques = (uniques)
+uniques = [i for i in uniques if i < 240]
+
+# answer less than 242
+
+print(max(uniques))
+# plt.plot([i for i in range(len(uniques))],uniques)
+# plt.show()
