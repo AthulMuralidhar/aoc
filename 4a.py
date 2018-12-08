258259a74ec6f761648279d2f283aecfc1eb6d91
AthulMuralidhar <athul.m68@gmail.com>
2018-12-08 18:23:10 +0100

    gave up on 4a today XD

--- 4a.py
+++ 4a.py
@@ -13,12 +13,14 @@ with open('inp.txt','r') as file:
 
 data = sorted(data)
 sleep_time = []
+guard_names = []
 time_counter = 0
 
 for i in data:
     j = i[1]
     if j.find("Guard") is not -1:
         name = j
+        guard_names.append(j)
     if j.find("falls asleep") is not -1:
         start_min = i[0].time()
         time_counter = i[0]
@@ -27,6 +29,23 @@ for i in data:
         sleep_time.append((name,start_min,time_counter))
         time_counter = 0
 
-sleep_delta = [i[2] for i in sleep_time]
-print(np.argmax(sleep_delta))
-print(sleep_time[52])
+# print(sleep_time)
+guard_names = set(guard_names)
+# print(guard_names)
+cumulative_sleep_time = []
+
+for i in guard_names:
+    cum_sum = datetime.now()-datetime.now()
+    for j,k,l in sleep_time:
+        if i == j:
+            cum_sum +=l
+    cumulative_sleep_time.append((i,cum_sum))
+
+# print(max(cumulative_sleep_time)) #guard 983
+
+for i,j,k in sleep_time:
+    if i.find('Guard #983') is not -1:
+        print(i,j,k)
+# sleep_delta = [i[1] for i in cumulative_sleep_time]
+# print(np.argmax(sleep_delta))
+# print(cumulative_sleep_time[18])
