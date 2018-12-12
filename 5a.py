8b1a2675a135c47ea34bdf9edf4c67741356b2d3
Kees van Ekeren <kees.van.ekeren@mobpro.com>
2018-12-12 23:16:37 +0100

    Some improvements

--- 5a.py
+++ 5a.py
@@ -2,22 +2,22 @@
 def breaker(data):
     # assumes data is a string type variable
     len_data = len(data)
-    for i,j in enumerate(data):
-        if j.upper() == data[(i+1)%len_data] or j.lower() == data[(i+1)%len_data]:
-            return data[:i]+data[(i+2):]
+    for i, j in enumerate(data):
+        if (j.islower() and j.upper() == data[i + 1 % len_data]) or (j.isupper() and j.lower() == data[i + 1 % len_data]):
+            return data[:i] + data[i + 2:]
 
 data1 = []
-with open('input.txt','r') as file:
+with open('input.txt', 'r') as file:
     for i in file:
         data1.append(i.strip())
 
 # print(data)
 data = "".join(data1)
-print(len(data) )
+print(len(data))
 
 while True:
-    res = data
     data = breaker(data)
+    print(data)
     if not data:
         break
 
