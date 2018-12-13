5ad7291a4b0b077ece50c54acadfc9ec6a58f489
AthulMuralidhar <athul.m68@gmail.com>
2018-12-13 14:18:28 +0100

    i dont know what im committing to

--- 5a.py
+++ 5a.py
@@ -3,8 +3,9 @@ def breaker(data):
     # assumes data is a string type variable
     len_data = len(data)
     for i,j in enumerate(data):
-        if j.upper() == data[(i+1)%len_data] or j.lower() == data[(i+1)%len_data]:
-            return data[:i]+data[(i+2):]
+        if j != data[(i+1)%len_data]:
+            if j.upper() == data[(i+1)%len_data] or j.lower() == data[(i+1)%len_data]:
+                return data[:i]+data[(i+2):]
 
 data1 = []
 with open('input.txt','r') as file:
@@ -13,7 +14,7 @@ with open('input.txt','r') as file:
 
 # print(data)
 data = "".join(data1)
-print(len(data) )
+print(len(data))
 
 while True:
     res = data
