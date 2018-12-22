72d4a74ccf75dd85dd7bc91439d87458dae9c629
AthulMuralidhar <athul.m68@gmail.com>
2018-12-22 14:11:00 +0100

    done day 3 KVE

--- 3a.py
+++ 3a.py
@@ -1,23 +1,32 @@
 import re
+from collections import defaultdict
 
 counter = 0
 data = open('r_input.txt').read().splitlines()
 # print(file_ptr)
 rgx = re.compile(r"^#(\d+) @ (\d+),(\d+): (\d+)x(\d+)$")
-dict = {}
+dict = defaultdict(int)
 id_ans = 0
 
 for line in data:
     id,x,y,w,h = map(int,rgx.match(line).groups())
     for i in range(x,x+w):
         for j in range(y,y+h):
-            if not (i,j) in dict.keys():
-                dict[(i,j)] = id
-                continue
-            elif dict[(i,j)] < id:
-                dict[(i,j)] +=1
+            dict[(i,j)] +=1
 
-            if dict[(i,j)] == id:
-                id_ans = id
+
+for claim in data:
+    id, x, y, w, h = map(int, rgx.match(claim).groups())
+    overlap_index = None
+    for i in range(x, x + w):
+        for j in range(y, y + h):
+            if dict[(i, j)] > 1:
+                overlap_index = id
+                continue
+    if not overlap_index:
+        id_ans = id
+        break
+
+
 print(id_ans)
 # print(dict)
