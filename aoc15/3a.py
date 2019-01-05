3441caa45dfe69e249f3815f5299e35ecaeb85b1
AthulMuralidhar <athul.m68@gmail.com>
2019-01-05 20:11:28 +0100

    d3a 2015

--- aoc15/3a.py
+++ aoc15/3a.py
@@ -1,11 +1,17 @@
 """
 this code works for the test cases
+sources:
+https://gitlab.com/gregor_ulm/advent_of_code_2015/blob/master/03/part1.py
 """
+from collections import defaultdict
+
 data = open('r_input.txt').read().splitlines()
 data = data[0]
 north_south_counter = 0
 east_west_counter = 0
-total_houses = 2
+
+houses = defaultdict(int)
+houses[(north_south_counter,east_west_counter)] = 1
 
 for i in data:
     if i == '^':
@@ -17,10 +23,12 @@ for i in data:
     elif i == '<':
         east_west_counter -=1
 
-    if north_south_counter !=0 or east_west_counter !=0:
-        total_houses +=1
+    if (north_south_counter,east_west_counter) in houses.keys():
+        houses[(north_south_counter,east_west_counter)] +=1
+    else:
+        houses[(north_south_counter,east_west_counter)] = 1
 
-    if north_south_counter + east_west_counter == 0:
-        total_houses -=1
-
+total_houses = 0
+for _ in houses.keys():
+    total_houses +=1
 print(total_houses)
