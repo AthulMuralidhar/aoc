74704bb9ff632bd92d61cc2735a8e6d8a7cb35fe
AthulMuralidhar <athul.m68@gmail.com>
2019-01-22 20:05:50 +0100

    d4++

--- aoc15/4a.py
+++ aoc15/4a.py
@@ -1,29 +1,26 @@
 """
 sources:https://www.geeksforgeeks.org/md5-hash-python/
+sources:https://github.com/jjhelmus/adventofcode/blob/master/day04.py
 """
 import hashlib
 
 def hashgetter(string,int_part):
-    result = string+"".join(str(i) for i in int_part)
+    result = string+"".join(str(int_part))
     result = hashlib.md5(result.encode())
     result = result.hexdigest()
-    if result[:5] == '00000':
+    if result[:6] == '000000':
         return True
 
     return False
 
-def intchanger(string,int_part):
-    for i in  range(-1,-9,-1):
-        while int_part[-i-1] <= 9:
-            if hashgetter(string,int_part) == True:
-                print(hashgetter(string,int_part))
-                return int_part
-            elif hashgetter(string,int_part) == False:
-                int_part[-i-1] +=1
+def intchanger(string):
+    for i in  range(10000000):
+        if hashgetter(string,i) == True:
+            return i
+    return
 
 if __name__ == '__main__':
 
     string = 'bgvyzdsv'
-    int_part = [0,0,0,0,0,0,0,0]
 
-    print(intchanger(string,int_part))
+    print(intchanger(string))
